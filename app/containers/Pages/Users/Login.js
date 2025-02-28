463b456d7324fcb8304b4ccb5257af1c7ab21db1
macbookair <queziajesuino@gmail.com>
2025-02-27 23:30:03 -0400

    login

--- app/containers/Pages/Users/Login.js
+++ app/containers/Pages/Users/Login.js
@@ -10,7 +10,7 @@ function Login({ setIsAuthenticated = () => { } }) {
   const [valueForm, setValueForm] = useState(null);
   const { classes } = useStyles();
   const history = useHistory();
-  const API_URL = process.env.REACT_APP_API_URL?.replace(/\/$/, '') || 'http://62.72.63.137:3001/';
+  const API_URL = process.env.REACT_APP_API_URL?.replace(/\/$/, '') || 'https://portal.iecg.com.br';
     
   const submitForm = async (values) => {
     try {
