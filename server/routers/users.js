3911f65ff2b2513a82d46fa0ceb6a2838352f409
macbookair <queziajesuino@gmail.com>
2025-04-15 02:33:10 -0400

    a

--- server/routers/users.js
+++ server/routers/users.js
@@ -4,15 +4,11 @@ const express = require('express');
 const {getUsers,postUsers,getUserDetalhe,putUser} = require("../controllers/users")
 const autenticado = require('../middlewares/autenticado')
 
-//router.use(autenticado)
-
-
-
 // Configurar para aceitar JSON
 router.use(express.json());
 router.get('/', getUsers)
 router.get('/:id', getUserDetalhe)
 router.post('/', postUsers)
-router.put('/:id', putUser);
+router.put('/:id', putUser)
 
 module.exports= router
