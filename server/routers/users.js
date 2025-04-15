d2b04ff09739e5c88dacd7d48c905a81bd58a118
macbookair <queziajesuino@gmail.com>
2025-04-15 02:29:38 -0400

    acrescentando no server o put users

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,7 +1,7 @@
 const {Router} = require("express")
 const router = Router()
 const express = require('express');
-const {getUsers,postUsers,getUserDetalhe,postLogin} = require("../controllers/users")
+const {getUsers,postUsers,getUserDetalhe,putUser} = require("../controllers/users")
 const autenticado = require('../middlewares/autenticado')
 
 //router.use(autenticado)
@@ -13,5 +13,6 @@ router.use(express.json());
 router.get('/', getUsers)
 router.get('/:id', getUserDetalhe)
 router.post('/', postUsers)
+router.put('/:id', putUser);
 
 module.exports= router
