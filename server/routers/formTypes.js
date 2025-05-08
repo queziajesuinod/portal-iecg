fc34ef36d6a6e7e92bb549fea95e066e8b66ed1b
macbookair <queziajesuino@gmail.com>
2025-05-08 02:12:04 -0400

    ss

--- server/routers/formTypes.js
+++ server/routers/formTypes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const FormTypeController = require('../controllers/FormTypeController');
 const autenticado = require('../middlewares/autenticado');
 
-router.get('/', autenticado, FormTypeController.list);
+router.get('/', autenticado, FormTypeController.listar);
 
 module.exports = router;
