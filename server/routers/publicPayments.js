9f01537990dec62c274e2dd783f95bb11f1ec5ba
Quézia Jesuino <quezitabjq@gmail.com>
2025-09-11 09:18:06 -0400

    retirar routes
    Retirando as rotass.

--- server/routers/publicPayments.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PublicPaymentController = require('../controllers/publicPaymentController');
-
-router.get('/por-cpf/:cpf', PublicPaymentController.getPaymentsByCpf);
-
-module.exports = router;
