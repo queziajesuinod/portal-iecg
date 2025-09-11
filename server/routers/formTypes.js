91f2f91667452c44f1fe2da6311c673efb707cac
Quézia Jesuino <quezitabjq@gmail.com>
2025-09-11 09:18:06 -0400

    retirar routes
    Retirando as rotass.

--- server/routers/formTypes.js
+++
@@ -1,9 +0,0 @@
-// routes/formTypes.js
-const express = require('express');
-const router = express.Router();
-const FormTypeController = require('../controllers/FormTypeController');
-const autenticado = require('../middlewares/autenticado');
-
-router.get('/', autenticado, FormTypeController.listar);
-
-module.exports = router;
--- server/routers/publicaForms.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const FormController = require('../controllers/formController');
-
-router.get('/forms/:slug', FormController.getFormBySlug);
-
-module.exports = router;
