30e4b4653cd793cb033b267ec7fea5cc7763c2e5
Quézia Jesuino <quezitabjq@gmail.com>
2026-01-07 22:22:17 -0400

    Adiciona rota pública para listagem de campus

--- server/routers/publicStartRoutes.js
+++ server/routers/publicStartRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ApeloDirecionadoCelulaController = require('../controllers/apelodirecionadocelulaController');
 const CelulaPublicController = require('../controllers/celulaPublicController');
+const CampusController = require('../controllers/campus');
 
 const router = express.Router();
 
@@ -8,6 +9,8 @@ const router = express.Router();
 router.post('/direcionamentos', ApeloDirecionadoCelulaController.criar);
 // Rota publica para cadastrar uma celula (sem autenticacao)
 router.post('/celulas', CelulaPublicController.criar);
+// Rota publica de listagem de campus
+router.get('/campus', CampusController.listar);
 // Consulta publica de celula por email ou cel_lider
 router.get('/celulas/contato', CelulaPublicController.buscarPorContato);
 // Atualizacao publica de dados da celula
