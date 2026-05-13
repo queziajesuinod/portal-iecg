af952dbd627309fe5efc76ccb41d4c64af966196
Quézia Jesuino <quezitabjq@gmail.com>
2026-05-13 16:00:54 -0400

    feat: atualizar comentários para esclarecer rotas do webhook da Evolution API

--- server/routers/evolutionWebhookRoutes.js
+++ server/routers/evolutionWebhookRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const notificationController = require('../controllers/notificationController');
 
 // Webhook da Evolution API (público - sem autenticação)
+// Aceita /evolution e /evolution/NOME_DO_EVENTO (quando Webhook by Events está ativo)
 router.post('/evolution', notificationController.processarWebhook);
+router.post('/evolution/:event', notificationController.processarWebhook);
 
 module.exports = router;
